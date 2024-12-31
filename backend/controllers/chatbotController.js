1bfa170408eedfd35ebb52b750ce51791479e25c
martinlpc <martinlpacheco@gmail.com>
2024-12-31 15:11:39 -0300

    Refactor bot controller and add isOpen response

--- backend/controllers/chatbotController.js
+++ backend/controllers/chatbotController.js
@@ -6,6 +6,7 @@ const queries = {
     direccion: 'Nos encontramos en la calle Falsa 123',
     domicilio: 'Nos encontramos en la calle Falsa 123',
     menu: 'A continuación te muestro nuestro menú: ...',
+    hola: 'Hola! Somos SushiNular! Escribí "menu" para ver nuestra carta',
 }
 
 export const handleUserMessage = async (message) => {
@@ -15,34 +16,56 @@ export const handleUserMessage = async (message) => {
 
     // Buscar coincidencia en las claves de las queries
     const foundQuery = Object.keys(queries).find(query => lowerMessage.includes(query))
-    if (foundQuery) {
-        console.log(`[query] ${foundQuery}`)
-        if (foundQuery === 'menu') {
-            // Aquí se podría hacer una consulta a la base de datos
-            const menu = ['Hamburguesa', 'Pizza', 'Ensalada', 'Papas fritas'] // Test
-            return `${queries[foundQuery]}\n\n${menu.map((item, index) => `${index + 1}. ${item}`).join('\n')}`
-        }
+    if (foundQuery) return queryResponse(foundQuery)
 
-        // No pidieron menú, devolver respuesta predefinida
-        return queries[foundQuery]
-    }
 
     // Detectar si se está pidiendo una orden
     const orderMatch = lowerMessage.match(/(?:ordenar|quiero|pedir)\s*(\d+)\s*(.*)/i)
     console.log('orderMatch: ', orderMatch)
-    if (orderMatch) {
-        const [, quantity, product] = orderMatch
+    if (orderMatch) return orderResponse(orderMatch)
 
-        const newOrder = new Order({
-            product,
-            quantity,
-            timestamp: new Date()
-        })
-        await newOrder.save()
 
-        return `Perfecto! Agregamos ${quantity} ${product} a tu pedido`
-    }
+    // Detectar si se está preguntando si están abiertos en este momento
+    if (lowerMessage.includes('abierto' || 'abiertos')) return isOpenResponse()
+
 
     // Respuesta por defecto si no se detectaron coincidencias
     return "Lo siento, no entendí tu mensaje"
+}
+
+function queryResponse(query) {
+    console.log(`[query] ${query}`)
+    if (query === 'menu') {
+        // TODO: Consultar DB con menú real
+        const menu = ['Hamburguesa', 'Pizza', 'Ensalada', 'Papas fritas'] // Test
+        return `${queries[query]}\n\n${menu.map((item, index) => `${index + 1}. ${item}`).join('\n')}`
+    }
+
+    // No pidieron menú, devolver respuesta predefinida
+    return queries[query]
+}
+
+async function orderResponse(orderMatch) {
+    const [, quantity, product] = orderMatch
+
+    const newOrder = new Order({
+        product,
+        quantity,
+        timestamp: new Date()
+    })
+    await newOrder.save()
+
+    return `Perfecto! Agregamos ${quantity} ${product} a tu pedido`
+}
+
+function isOpenResponse() {
+    const now = new Date()
+    const day = now.getDay()
+    const hour = now.getHours()
+
+    if (day >= 2 && day <= 6 && hour >= 11 && hour < 23) {
+        return 'Sí, estamos abiertos hasta las 23 horas!'
+    } else {
+        return `Lo siento, estamos cerrados en este momento. ${queries.horario}`
+    }
 }
